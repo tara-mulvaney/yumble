51f160b24a1291d93e7ec67b14624ad579fb704a
Rachel Gonzalez <dancearchitect@gmail.com>
2019-05-18 11:09:54 -0400

    updated home styling

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./Home.css";
+import logo from "../Home/yumble_logo.png"
 
 function Home() {
   return (
     <div>
       <div className="parent">
         <div className="left">
-          <h1>YUMBLE</h1>
+          <h1><img src={logo}/></h1>
         </div>
         <div className="right">
           <div className="searchbar">
-            <input type="text" name="search-by-location" placeholder="Search for restaurants in your area!" />
+            <input type="text" id="searchLocation" placeholder="Delivery Address" />
           </div>
         </div>
       </div>
