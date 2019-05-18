b104152acbbc3279729b71167e6f53c82c6d2eff
Tara Mulvaney <mulvaneytara@gmail.com>
2019-05-18 13:27:01 -0400

    menu

--- client/src/components/RestaurantProfile/Menu.jsx
+++ client/src/components/RestaurantProfile/Menu.jsx
@@ -1,7 +1,175 @@
 import React from "react";
+import './Menu.css'
 
 function Menu() {
-  return <div />;
+  return (
+<div>
+  <header class="banner"></header>
+    <div class="box-1">
+    <div class="profile">
+      <h1>Maison Kayser</h1>
+      </div>
+      <div class="flex-around">
+        <p>*</p>
+        <p>*</p>
+        <p>*</p>
+        <p>*</p>
+        <p>*</p>
+      </div>
+    <div class="flex">
+      <p> image </p>
+      <p> image </p>
+      <p> image </p>
+      <p> image </p>
+      <p> image </p>
+    </div>
+    <div class="flex">
+      <p> $8
+        <button> + Add </button>
+      </p>
+      <p> $8
+        <button> + Add </button>
+      </p>
+      <p> $8
+        <button> + Add </button>
+      </p>
+       <p> $8
+        <button> + Add </button>
+      </p>
+       <p> $8
+        <button> + Add </button>
+      </p>
+    </div>
+    <div class="flex">
+      <p>Avocado Toast</p>
+      <p>Chocolate Cake</p>
+      <p>Jam Donut</p>
+      <p>Butter Crepe</p>
+      <p>Millefeuille</p>
+    </div>
+    </div>
+    <div class="box-1">
+      <h2>Pastries</h2>
+    <div class="flex-around">
+    <div class="flex-column">
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+    </div>
+      <div class="flex-column">
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+    </div>
+      <div class="flex-column">
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+    </div>
+      <div class="flex-column">
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+    </div>
+    </div>
+    </div>
+    <div class="box-1">
+      <h2>Mains</h2>
+    <div class="flex-around">
+    <div class="flex-column">
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+    </div>
+      <div class="flex-column">
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+    </div>
+      <div class="flex-column">
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+    </div>
+      <div class="flex-column">
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+    </div>
+    </div>
+    </div>
+    <div class="box-1">
+      <h2>Desserts</h2>
+    <div class="flex-around">
+    <div class="flex-column">
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+    </div>
+      <div class="flex-column">
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+    </div>
+      <div class="flex-column">
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+        <p>Menu Item</p>
+    </div>
+      <div class="flex-column">
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+        <p>$8 +</p>
+    </div>
+    </div>
+    </div>
+    <div class="box-1">
+      <h2>Mains</h2>
+    <div class="flex-around">
+    <div class="flex-column">
+        <p>Menu Item</p>
+    </div>
+      <div class="flex-column">
+        <p>$8 +</p>
+    </div>
+      <div class="flex-column">
+        <p>Menu Item</p>
+    </div>
+      <div class="flex-column">
+        <p>$8 +</p>
+    </div>
+    </div>
+  </div>
+</div>
+  )
 }
 
 export default Menu;
