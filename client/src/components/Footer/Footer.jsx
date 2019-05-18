2e234c0de8ee123ae7f327a68e0a80b8be728256
Tara Mulvaney <mulvaneytara@gmail.com>
2019-05-18 15:01:05 -0400

    footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,8 +3,28 @@ import './Footer.css'
 
 function Footer() {
   return (
-    <div>
-    </div>
+<div class="flex-around">
+  <div class="flex-column">
+    <h2>Get To Know Us</h2>
+      <a href="/">Text</a>
+      <a href="/">Text</a>
+      <a href="/">Text</a>
+      <a href="/">Text</a>
+  </div>
+  <div class="flex-column">
+    <h2>Business Solutions</h2>
+      <a href="/">Text</a>
+      <a href="/">Text</a>
+      <a href="/">Text</a>
+      <a href="/">Text</a>
+  </div>
+    <div class="flex-column">
+       <h2>Useful Links</h2>
+      <a href="/">Text</a>
+      <a href="/">Text</a>
+      <a href="/">Text</a>
+  </div>
+</div>
   )
 }
 
