6fa5000e0eef8135e4883f05f0bbbfae83fadded
Rachel Gonzalez <dancearchitect@gmail.com>
2019-05-18 13:57:59 -0400

    slight edit to home styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Home />
+      </div>
+    );
+  }
 }
 
 export default App;
